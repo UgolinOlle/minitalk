1ee18b9936ed8c5cc71e42f1ffbe3a5462520613
Ugolin Ollé <hello@ugolin-olle.com>
2023-11-25 19:36:32 +0700

    fix: 🐛 update function name

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,15 +6,18 @@
 /*   By: ugolin-olle <ugolin-olle@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 11:08:51 by ugolin-olle       #+#    #+#             */
-/*   Updated: 2023/11/21 13:23:56 by ugolin-olle      ###   ########.fr       */
+/*   Updated: 2023/11/25 14:38:14 by ugolin-olle      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
+// -- Custom librabry
 # include "../libs/includes/libft.h"
+
+// -- Libraries
 # include <signal.h>
-# include <unistd.h>
+# include <stdio.h>
 
 #endif
--- libs/srcs/misc/ft_handle_error.c
+++ libs/srcs/misc/ft_handle_error.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_error.c                                         :+:      :+:    :+:   */
+/*   ft_handle_error.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ugolin-olle <ugolin-olle@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 14:00:08 by ugolin-olle       #+#    #+#             */
-/*   Updated: 2023/11/25 14:03:47 by ugolin-olle      ###   ########.fr       */
+/*   Updated: 2023/11/25 14:51:48 by ugolin-olle      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_handle_errors(char *content)
+void	ft_handle_error(char *content)
 {
 	ft_putstr_fd(content, STDOUT_FILENO);
 	ft_putstr_fd("\n", STDOUT_FILENO);
