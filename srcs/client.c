ac3c1166237fbe0295c0b30a5afbeeafe79cd319
Ugolin Ollé <hello@ugolin-olle.com>
2023-11-25 20:22:26 +0700

    fix: 🐛 receive and build message with bit

--- srcs/client.c
+++ srcs/client.c
@@ -6,10 +6,158 @@
 /*   By: ugolin-olle <ugolin-olle@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 17:40:26 by ugolin-olle       #+#    #+#             */
-/*   Updated: 2023/11/21 11:06:43 by ugolin-olle      ###   ########.fr       */
+/*   Updated: 2023/11/25 19:50:50 by ugolin-olle      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_check_args(int argc, char **argv)
+/**
+ * @file client.c
+ * @brief Client program for the Minitalk project.
+ *
+ * This file contains the client code for the Minitalk project.
+
+	* The client sends messages to the server in the form of UNIX signals (SIGUSR1 and SIGUSR2),
+ * encoding the characters of the message into bits.
+ */
+
+#include "../includes/minitalk.h"
+
+/**
+ * @brief Checks the program arguments.
+ *
+ * Verifies that the client program is launched with the correct arguments.
+ * In case of error, displays a message and terminates the program.
+ *
+ * @param argc Number of arguments.
+ * @param argv Array of arguments.
+ */
+static void	ft_args_check(int argc, char **argv)
 {
+	int	i;
+
+	i = 0;
+	if (argc != 3)
+	{
+		ft_putstr_fd("An error occured while use client. Wrong format.",
+			STDOUT_FILENO);
+		ft_putstr_fd("./client <PID> <MESSAGE>", STDOUT_FILENO);
+		exit(EXIT_FAILURE);
+	}
+	while (argv[1][i])
+	{
+		if (!ft_isdigit(argv[1][i]))
+		{
+			ft_putstr_fd("An error occured, PID is wrong.", STDOUT_FILENO);
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+	if (*argv[2] == 0)
+	{
+		ft_putstr_fd("An error occured, message should not be empty.",
+			STDOUT_FILENO);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * @brief Sends the bits of a message to the server.
+ *
+ * Sends each character of the message as bits to the server,
+ * using SIGUSR1 (for bit 1) and SIGUSR2 (for bit 0) signals.
+ *
+ * @param pid PID of the server process.
+ * @param content Message to send.
+ */
+static void	ft_send_bits(pid_t sv_pid, char *content)
+{
+	unsigned char	c;
+	int				bits;
+
+	while (*content)
+	{
+		c = *content;
+		bits = 8;
+		while (bits--)
+		{
+			if (c & 0b10000000)
+				kill(sv_pid, SIGUSR1);
+			else
+				kill(sv_pid, SIGUSR2);
+			usleep(50);
+			c <<= 1;
+		}
+		content++;
+	}
+}
+
+/**
+ * @brief Signal handler for the client.
+ *
+ * Handles the signals received by the client, notably SIGUSR2,
+ * indicating that the message has been successfully sent.
+ *
+ * @param signum Signal number received.
+ */
+static void	ft_handler(int signum, siginfo_t *sv_info, void *context)
+{
+	if (signum == SIGUSR2)
+		ft_putstr_fd("[CLIENT] - Character as been successfully sent.\n",
+			STDOUT_FILENO);
+}
+
+/**
+ * @brief Configures the client for signal handling.
+ *
+ * Sets up signal handling for the client,
+ * defining the actions to take for SIGUSR1 and SIGUSR2.
+ */
+static void	ft_config_client(void)
+{
+	struct sigaction	sa_client;
+
+	sa_client.sa_sigaction = &ft_handler;
+	sa_client.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa_client.sa_mask);
+	if (sigaction(SIGUSR1, &sa_client, NULL) < 0)
+	{
+		ft_putstr_fd("[CLIENT] - Error: SIGUSR1 asn't been set.",
+			STDOUT_FILENO);
+		exit(EXIT_FAILURE);
+	}
+	else if (sigaction(SIGUSR2, &sa_client, NULL) < 0)
+	{
+		ft_putstr_fd("[CLIENT] - Error: SIGUSR2 asn't been set.",
+			STDOUT_FILENO);
+		exit(EXIT_FAILURE);
+	}
+	ft_putstr_fd("[CLIENT] - Client as been configured correctly.",
+		STDOUT_FILENO);
+	ft_putstr_fd("\n", STDOUT_FILENO);
+}
+
+/**
+ * @brief Main function of the client program.
+ *
+ * Initializes the client, checks the arguments, sends a message to the server,
+ * and indefinitely waits for the reception of signals.
+ *
+ * @param argc Number of arguments.
+ * @param argv Array of arguments.
+ * @return Always returns EXIT_SUCCESS.
+ */
+int	main(int argc, char **argv)
+{
+	pid_t	pid;
+
+	ft_args_check(argc, argv);
+	pid = ft_atoi(argv[1]);
+	ft_putstr_fd("[CLIENT] - PID:", STDOUT_FILENO);
+	ft_putnbr_fd(getpid(), STDOUT_FILENO);
+	ft_putstr_fd("\n", STDOUT_FILENO);
+	ft_config_client();
+	ft_send_bits(pid, argv[2]);
+	while (1)
+		pause();
+	return (EXIT_SUCCESS);
 }
--- srcs/server.c
+++ srcs/server.c
@@ -6,59 +6,95 @@
 /*   By: ugolin-olle <ugolin-olle@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 17:43:37 by ugolin-olle       #+#    #+#             */
-/*   Updated: 2023/11/24 15:05:38 by ugolin-olle      ###   ########.fr       */
+/*   Updated: 2023/11/25 19:56:01 by ugolin-olle      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @file server.c
+ * @brief Server program for Minitalk project.
+ *
+ * This file contains the server code for the Minitalk project.
+	The server listens for signals (SIGUSR1 and SIGUSR2) sent by a Minitalk client,
+ * and reconstructs the message bit by bit. Once a full character is received,
+ * it is printed to the standard output.
+ */
+
 #include "../includes/minitalk.h"
 
-void	ft_server_config(struct sigaction *s_server)
+/**
+ * @brief Gestionnaire de signal pour le serveur.
+ *
+ * Cette fonction est appelée lorsqu'un signal est reçu par le serveur.
+ * Elle reconstruit les caractères à partir des bits envoyés par le client.
+ * Après la réception d'un caractère complet,
+	celui-ci est imprimé sur la sortie standard.
+ * Si un caractère nul est reçu, une notification "Message reçu" est imprimée.
+ *
+ * @param signum Le numéro du signal reçu (SIGUSR1 ou SIGUSR2).
+ * @param sv_info Informations sur le signal, y compris le PID de l'expéditeur.
+ * @param context Pointeur vers un contexte,
+	non utilisé dans cette fonction (peut être ignoré).
+ */
+static void	ft_handler(int signum, siginfo_t *info, void *ucontent)
 {
-	if (sigaction(SIGUSR1, s_server, NULL) < 0)
-	{
-		ft_putstr_fd("[SERVER] - Error: SIGUSR1 asn't been set.",
-			STDOUT_FILENO);
-		exit(EXIT_FAILURE);
-	}
-	else if (sigaction(SIGUSR2, s_server, NULL) < 0)
-	{
-		ft_putstr_fd("[SERVER] - Error: SIGUSR2 asn't been set.",
-			STDOUT_FILENO);
-		exit(EXIT_FAILURE);
-	}
-}
+	static int				bits = -1;
+	static unsigned char	c;
 
-static void	ft_handler(int signum)
-{
-	static int	bits;
-	static char	message;
-
-	bits = 0;
-	message = 0;
+	(void)ucontent;
+	if (bits < 0)
+		bits = 7;
 	if (signum == SIGUSR1)
-		message |= 1 << (((sizeof(char) * 8) - 1) - bits);
-	bits++;
-	if (bits == 8)
+		c |= (1 << bits);
+	bits--;
+	if (bits < 0 && c)
 	{
-		if (message == '\0')
-			ft_putstr_fd("Message received", STDOUT_FILENO);
-		else
-			ft_putstr_fd(&message, STDOUT_FILENO);
-		bits = 0;
-		message = 0;
+		if (c == '\0')
+			ft_putstr_fd("\n", STDOUT_FILENO);
+		ft_putchar_fd(c, STDOUT_FILENO);
+		c = 0;
+		if (kill(info->si_pid, SIGUSR2) == -1)
+			ft_handle_error("Server failed to send SIGUSR2");
+		return ;
 	}
+	if (kill(info->si_pid, SIGUSR1) == -1)
+		ft_handle_error("Failed to send SIGUSR1");
 }
 
+/**
+ * @brief Configures the server to handle signals.
+ *
+ * Sets up the signal handling for SIGUSR1 and SIGUSR2 using sigaction.
+ * In case of failure, prints an error message and exits.
+ */
+static void	ft_config_server(void)
+{
+	struct sigaction	sa_server;
+
+	sa_server.sa_sigaction = &ft_handler;
+	sa_server.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa_server.sa_mask);
+	if (sigaction(SIGUSR1, &sa_server, NULL) < 0)
+		ft_handle_error("[SERVER] - Error: SIGUSR1 asn't been set.");
+	else if (sigaction(SIGUSR2, &sa_server, NULL) < 0)
+		ft_handle_error("[SERVER] - Error: SIGUSR2 asn't been set.");
+}
+
+/**
+ * @brief Main function of the server program.
+ *
+ * Initializes the server, displays its PID, and waits for signals.
+ * Upon receiving signals, processes them to reconstruct and print messages.
+ *
+ * @return Always returns EXIT_SUCCESS.
+ */
 int	main(void)
 {
-	struct sigaction	s_server;
-
 	ft_putstr_fd("[SERVER] - PID: ", STDOUT_FILENO);
 	ft_putnbr_fd(getpid(), STDOUT_FILENO);
-	s_server.sa_handler = &ft_handler;
-	s_server.sa_flags = SA_SIGINFO | SA_RESETHAND;
-	ft_server_config(&s_server);
+	ft_putstr_fd("\n[SERVER] - Waiting for message...\n", STDOUT_FILENO);
+	ft_config_server();
 	while (1)
 		pause();
-	return (0);
+	return (EXIT_SUCCESS);
 }
