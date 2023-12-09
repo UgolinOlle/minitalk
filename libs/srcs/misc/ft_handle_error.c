f46f6877c138581ee4a6307d155d47d9d5a054ee
Ugolin Ollé <hello@ugolin-olle.com>
2023-12-09 21:33:46 +0700

    fix: 🐛 update header path

--- libs/srcs/misc/ft_handle_error.c
+++ libs/srcs/misc/ft_handle_error.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../../includes/libft.h"
 
 void	ft_handle_error(char *content)
 {
